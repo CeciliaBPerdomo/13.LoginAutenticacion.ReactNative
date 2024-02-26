67159ed79b7062370c4a8ec84b9f791fc5befaeb
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-02-26 11:35:59 -0300

    Tabs

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -6,13 +6,41 @@ import ShopStack from './ShopStack'
 
 // Bottoms Tabs
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CartStack from './CartStack';
 
+import colors from '../utils/global/colors';
+import OrderStack from './OrderStack';
+
+const Tab = createBottomTabNavigator();
 
 const MainNavigation = () => {
     return (
         <>
             <NavigationContainer>
-                <ShopStack />
+                <Tab.Navigator
+                    initialRouteName='ShopStack'
+                    screenOptions={{
+                        headerShown: false,         // No muestra el encabezado del tab
+                        tabBarShowLabel: false,     // No muestra el texto del tab
+                        tabBarStyle: styles.tabBar
+                    }}
+                >
+                    <Tab.Screen
+                        name="ShopStack"
+                        component={ShopStack}
+                    />
+
+                    <Tab.Screen
+                        name="CartStack"
+                        component={CartStack}
+                    />
+
+                    <Tab.Screen
+                        name="OrderStack"
+                        component={OrderStack}
+                    />
+
+                </Tab.Navigator>
             </NavigationContainer>
         </>
     )
@@ -20,4 +48,10 @@ const MainNavigation = () => {
 
 export default MainNavigation
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    tabBar: {
+        backgroundColor: colors.green1,
+        height: 80,
+        shadowColor: "black"
+    }
+})
